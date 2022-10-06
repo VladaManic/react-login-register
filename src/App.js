35cf82907e4f97994f46e06f5b3cc0b0f7ebc921
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2022-10-06 11:32:49 +0200

    routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+
 import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <main className="App">
-      <Register />
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      
     </main>
   );
 }
