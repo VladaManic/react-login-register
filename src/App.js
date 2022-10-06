e90bc1f71513f89796f4d4cdee83f900c4f0da11
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2022-10-06 14:03:05 +0200

    Login and Home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import Register from './components/Register';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
-      
     </main>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
-import { React, useRef, useState } from 'react'
-import { Link } from 'react-router-dom';
+import { React, useRef, useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+import Home from './Home';
 
 const Login = () => {
+	const navigate = useNavigate();
 	const errRef = useRef();
 
 	const [user, setUser] = useState('');
@@ -9,48 +12,69 @@ const Login = () => {
 	const [errMsg, setErrMsg] = useState('');
 	const [success, setSuccess] = useState(false);
 
+	useEffect(() => {
+		setErrMsg('');
+	}, [user, pwd])
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		console.log(user, pwd);
+		setUser('');
+		setPwd('');
+		setSuccess(true);
+		//navigate('/home')
+	}
+
 	return (
-		<section>
-			<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
-			<h1>Log in</h1>
-			<form>
-				<div>
-					<label htmlFor="username">
-						Username:
-					</label>
-					<input
-						type="text"
-						id="username"
-						//ref={userRef}
-						autoComplete="off"
-						onChange={(e) => setUser(e.target.value)}
-						value={user}
-						required
-					/>
-				</div>
+		<>
+			{success ? (
+				<section>
+					<h1>You are now logged in!</h1>
+				</section>
+			) : (
+				<section>
+					<p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>{errMsg}</p>
+					<h1>Log in</h1>
+					<form onSubmit={handleSubmit}>
+						<div>
+							<label htmlFor="username">
+								Username:
+							</label>
+							<input
+								type="text"
+								id="username"
+								//ref={userRef}
+								autoComplete="off"
+								onChange={(e) => setUser(e.target.value)}
+								value={user}
+								required
+							/>
+						</div>
 
-				<div>
-					<label htmlFor="password">
-						Password:
-					</label>
-					<input
-						type="password"
-						id="password"
-						onChange={(e) => setPwd(e.target.value)}
-						value={pwd}
-						required
-					/>
-				</div>
+						<div>
+							<label htmlFor="password">
+								Password:
+							</label>
+							<input
+								type="password"
+								id="password"
+								onChange={(e) => setPwd(e.target.value)}
+								value={pwd}
+								required
+							/>
+						</div>
 
-				<button>Sign In</button>
-			</form>
-			<p>
-				Not registered?<br />
-				<span className="line">
-					<Link to='/register'>Sign Up</Link>
-				</span>
-			</p>
-		</section>
+						<button>Sign In</button>
+					</form>
+					<p>
+						Not registered?<br />
+						<span className="line">
+							<Link to='/register'>Sign Up</Link>
+						</span>
+					</p>
+				</section>
+			)}
+		</>
 	)
 }
 
