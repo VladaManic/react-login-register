7d42cbce058010a60e96b63bc74eafac63d6c938
Vladimir Manić <65961459+VladaManic@users.noreply.github.com>
2022-10-10 12:40:53 +0200

    if no one loged in, redirect from dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { AuthProvider } from './contexts/AuthContext'
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -12,7 +13,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }></Route>
         </Routes>
       </main>
     </AuthProvider>
